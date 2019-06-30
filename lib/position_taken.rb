e8ba5956a726b373c7d965fc2ca7d1615b560e24
Peter Sampson <petersampson010@gmail.com>
2019-06-30 08:47:12 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[${index}] == "X" || "O"
     return true 
-  elsif board[index] == " " || "" || nil
+  elsif board[${index}] == " " || "" || nil
     return false
   end
 end
